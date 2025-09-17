d363bad7201daaee12c98f05ccf7333596f86f0f
Your Name <your.email@example.com>
2025-09-17 14:43:18 +0100

    Feature: created a topbar and sidebar for the dashboard homepage

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,27 @@ import Mainlayout from "./layouts/Mainlayout";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
+import DashboardHomepage from "./pages/DashboardHomepage";
 import { Route, Routes } from "react-router-dom";
 import { AnimatePresence } from "framer-motion";
 import NotFoundPage from "./pages/NotFoundPage";
+import Dashboardlayout from "./layouts/Dashboardlayout";
 
 function App() {
   return (
     <AnimatePresence mode="wait">
-    <Routes>
-      {/* Public pages routes */}
-      <Route element={<Mainlayout/>}>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/register" element={<RegisterPage />} />
-      
-      </Route>
-      <Route path="*" element={<NotFoundPage />} />
-    </Routes>
+      <Routes>
+        {/* Public pages routes */}
+        <Route element={<Mainlayout />}>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+        </Route>
+        <Route element={<Dashboardlayout />}>
+          <Route path="/dashboard" element={<DashboardHomepage/>}/>
+        </Route>
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
     </AnimatePresence>
   );
 }
---
+++ src/pages/DashboardHomepage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const DashboardHomepage = () => {
+  return (
+    <>
+    <div className='text-6xl p-4 uppercase text-primary-300'>
+        <p >Dashboard Homepage</p>
+        </div>
+    </>
+  )
+}
+
+export default DashboardHomepage
